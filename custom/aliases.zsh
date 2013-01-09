155c4fabb8209219ea4a28e8942abbde2d323856
rajeshgaikwad <rajesh.gaikwad@gmail.com>
2013-01-09 16:14:56 +0530

    Some more tweaks

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -13,8 +13,10 @@ alias sites='cd ~/Sites'
 alias log_cleanup='sudo rm -rf /private/var/log/asl/*'
 
 # projects
-alias odap='cd ~/Sites/addonmobi.odap-dashboard'
+alias odap='cd ~/Sites/odap'
 alias temp='cd ~/Sites/temp3'
+alias s='cd ~/Sites/'
+alias v='vim'
 
 
 ## Git Aliases
@@ -30,11 +32,23 @@ alias got='git '
 alias gpush='git push origin master'
 
 
-# ls
+# Unix
+alias tlf="tail -f"
 alias l='ls -lAhrt'
 alias ll='ls -lart'
 alias la='ls -Art'
 alias ls='ls -lsrt'
+alias path='echo -e ${PATH//:/\\n}'
+alias now='date +"%T'
+alias nowtime=now
+alias nowdate='date +"%d-%m-%Y"'
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
+
+# File size
+alias fs="stat -f \"%z bytes\""
+ 
 
 #alias -s
 alias -s erb=vim
@@ -43,9 +57,17 @@ alias -s scss=vim
 alias -s html=vim
 
 # interactive/verbose commands
-alias df='df -h'
+alias df='df -ah'
 alias mv='mv -i'
 alias rm='rm -i'
+
 for c in cp rm chmod chown; do
       alias $c="$c -v"
   done
+#rails
+alias brs="bundle exec rails s"
+alias brc="bundle exec rails c"
+
+alias railss="rails s"
+alias railsc="rails c"
+alias railsdb="rails dbconsole"
